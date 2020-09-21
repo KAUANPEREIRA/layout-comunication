dab78df1cd6070da68ebcf0709153ba30993a3e5
KAUANPEREIRA <kauanauan71@gmail.com>
2020-09-21 18:40:08 -0300

    atualizacao 21/09
    atualização do projeto responsivo utilizando flex-box

---
+++ script.js
@@ -0,0 +1,11 @@
+let pessoa ={nome:'kauan',sobrenome:'pereira',idade:'18'}
+
+let maisDados ={
+    ...pessoa,
+    profissao:'programador',
+    carro:'corsa',
+    formado:'ainda n'
+
+}
+
+console.log(maisDados)
